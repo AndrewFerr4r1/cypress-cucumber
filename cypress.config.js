350fa203d70c102fc1e98bd292ded0edf088d6aa
Andrew Ferrari <andrew.ferrari@sptech.school>
2024-09-28 15:11:34 -0300

    config ci

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber())
+      on('file:preprocessor', cucumber());
+      return config;
     },
     specPattern: "cypress/e2e/features/*.feature"
   },
